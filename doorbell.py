ea117dcddd700eae54b25b008ca59a5ad1121def
this-post <displaymyname@gmail.com>
2017-07-07 22:39:57 +0700

    modified:   doorbell.py

--- doorbell.py
+++ doorbell.py
@@ -1,31 +1,50 @@
 import RPi.GPIO as gpio
-import time
-
-# Board PIN numbering
-dataPin = 12
-gpio.setmode(gpio.BOARD)
-gpio.setup(dataPin, gpio.OUT)
+import time, argparse
 
 def delay(is_long):
 	time.sleep(0.0005) if is_long else time.sleep(0.00025)
 
-#long = 1, short = 0
-times = [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 
-	1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 
-	0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0]
+def ring():
+	#long = 1, short = 0
+	times = [1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 
+		1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 
+		0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 1, 0]
+	for i in range(75):
+		logic = 1
+		for timing in times:
+			if logic:
+				gpio.output(dataPin, 1)
+				delay(timing)
+				logic = 0
+			else:
+				gpio.output(dataPin, 0)
+				delay(timing)
+				logic = 1
+		#last wave ending with longer 0 than usual
+		gpio.output(dataPin, 0)
+		time.sleep(0.005)
 
-for i in range(75):
-	logic = 1
-	for timing in times:
-		if logic:
+def force_pairing():
+	for i in range(4):
+		time.sleep(2)
+		for j in range(75):
 			gpio.output(dataPin, 1)
-			delay(timing)
-			logic = 0
-		else:
+			time.sleep(0.01)
 			gpio.output(dataPin, 0)
-			delay(timing)
-			logic = 1
-	#last wave ending with longer 0 than usual
-	gpio.output(dataPin, 0)
-	time.sleep(0.005)
-gpio.cleanup()
+			time.sleep(0.005)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Ringing Advante\'s wireless doorbell')
+	parser.add_argument('-ring', dest='action', action='store_const', const='ring()', help='Ringing')
+	parser.add_argument('--force-pairing', dest='action', action='store_const', const='force_pairing()', help='Force pairing when doorbell is unpaired')
+	arg = parser.parse_args()
+	if arg.action is None:
+		parser.print_help()
+		exit(0)
+	else:
+		#Board PIN numbering
+		dataPin = 12
+		gpio.setmode(gpio.BOARD)
+		gpio.setup(dataPin, gpio.OUT)
+		exec arg.action
+		gpio.cleanup()
